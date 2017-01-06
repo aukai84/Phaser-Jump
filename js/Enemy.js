5d69dffbf5504d4f0b4ba7f5d31b230117ede6a1
Aukai Tirrell <mtirrell@hawaii.edu>
2017-01-05 20:47:27 -1000

    added enemy module

---
+++ js/Enemy.js
@@ -0,0 +1,15 @@
+//jshint esversion: 6
+((Phaser, Game, CFG) => {
+
+  if(Game === undefined){
+    Game = window.Game = {};
+  }
+
+  Game.Enemy = class{
+    constructor(){
+
+    }
+  };
+
+
+})(window.Phaser, window.Game, window.Game.Configuration);
